e0a85a68fbf46b2372ad1ccaec30cd05444e455b
Jason Nguyen <jnguyen7473@gmail.com>
2025-03-30 20:41:20 -0700

    Added recipe model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,21 @@
+import datetime
+
 from app import db
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32))
     password = db.Column(db.String(32))
     email = db.Column(db.String(32))
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(80))
+    description = db.Column(db.String())
+    ingredients = db.Column(db.String())
+    instructions = db.Column(db.String())
+    created = db.Column(db.String())
+
