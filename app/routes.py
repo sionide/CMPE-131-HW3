3162093acd125d42f0c2b087cb621889bd3565b4
Jason Nguyen <jnguyen7473@gmail.com>
2025-03-30 20:43:56 -0700

    Added a new endpoint for making Recipes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
+import datetime
 from app import myapp_obj
 from flask import render_template
 from flask import redirect
-from app.forms import LoginForm
-from app.models import User
+from app.forms import LoginForm, RecipeForm
+from app.models import User, Recipe
 from app import db
 # from <X> import <Y>
 
@@ -26,7 +27,26 @@ def login():
         db.session.commit()
         return redirect("/")
     else:
-        print("MOOOO MOOO")
+        print("MOOOO MOOO BAD")
     return render_template("login.html", form=form)
     # What is render template returning?
     #return str(type(render_template("login.html", form=form)))
+
+@myapp_obj.route("/recipe/new", methods=['GET', 'POST'])
+def create_recipe():
+    form = RecipeForm()
+    if form.validate_on_submit():
+        print(f"Here is the input from the user {form.title.data}")
+        recipe = Recipe(title=form.title.data,
+                        user_id=form.user_id.data,
+                        description=form.description.data,
+                        ingredients=form.ingredients.data,
+                        instructions=form.instructions.data,
+                        created=datetime.datetime.now())
+
+        db.session.add(recipe)
+        db.session.commit()
+        return redirect("/")
+    else:
+        print("MOOOO MOOO BAD")
+    return render_template("recipe_new.html", form=form)
