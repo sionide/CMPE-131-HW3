97441085eb46fa61a345e15a0a18822c2f9d7bf2
Jason Nguyen <jnguyen7473@gmail.com>
2025-03-30 20:43:03 -0700

    Added a new form for Recipes

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[validators.Length(min=4, max=35)])
     submit =  SubmitField("Sign in")
     remember_me = BooleanField("Remember Me")
+
+class RecipeForm(FlaskForm):
+    user_id = StringField("ID Number", validators=[validators.DataRequired()])
+    title = StringField("Title", validators=[validators.Length(min=1, max=80)])
+    description = StringField("Description", validators=[validators.DataRequired()])
+    ingredients = StringField("Ingredients", validators=[validators.DataRequired()])
+    instructions = StringField("Instructions", validators=[validators.DataRequired()])
+    submit_recipe =  SubmitField("submit recipe")
+
